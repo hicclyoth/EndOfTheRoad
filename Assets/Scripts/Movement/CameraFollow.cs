b1c83ece8e85e82cab98b84f06a57a3c8a66c524
Clyoth <sumiplorem@gmail.com>
2025-04-09 02:37:42 +0800

    Added multiple states for camera
    - Static or Follow

--- Assets/Scripts/Movement/CameraFollow.cs
+++ Assets/Scripts/Movement/CameraFollow.cs
@@ -2,13 +2,29 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public float camSpeed = 1.5f;
-    public Transform target;
+    public float camSpeed = 1.5f;      // Camera movement speed
+    public Transform target;           // The target (player) the camera will follow
+    public bool followPlayer = false;  // Flag to control whether the camera should follow the player
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 pos = new Vector3(target.position.x, target.position.y, -10f);
-        transform.position = Vector3.Slerp(transform.position, pos, camSpeed);
+        // If followPlayer is true, move the camera
+        if (followPlayer && target != null)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, -10f);
+            transform.position = Vector3.Slerp(transform.position, targetPosition, camSpeed * Time.deltaTime);
+        }
+    }
+
+    // Method to start camera follow (to be called from ProximityDetector or any other trigger)
+    public void StartFollowing()
+    {
+        followPlayer = true;
+    }
+
+    // Method to stop camera follow
+    public void StopFollowing()
+    {
+        followPlayer = false;
     }
 }
