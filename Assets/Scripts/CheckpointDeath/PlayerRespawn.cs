9427a929469547ac3a961a066f16d087f4f0edcb
Clyoth <sumiplorem@gmail.com>
2025-04-09 18:50:46 +0800

    Created a temporary death screen
    - Will be replaced by a death animation instead of just pausing

--- Assets/Scripts/CheckpointDeath/PlayerRespawn.cs
+++ Assets/Scripts/CheckpointDeath/PlayerRespawn.cs
@@ -1,12 +1,23 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     private Vector2 lastCheckpointPosition;
 
+    [Header("UI")]
+    [SerializeField] private GameObject respawnPanel;
+
     private void Start()
     {
         lastCheckpointPosition = transform.position;
+
+        // Make sure panel is hidden on start
+        if (respawnPanel != null)
+        {
+            respawnPanel.SetActive(false);
+        }
     }
 
     public void SetCheckpoint(Vector2 checkpointPosition)
@@ -16,7 +27,47 @@ public class Player : MonoBehaviour
 
     public void RespawnPlayer()
     {
-        transform.position = lastCheckpointPosition;
+        Time.timeScale = 0f;
+        StartCoroutine(RespawnRoutine());
     }
 
+    private IEnumerator RespawnRoutine()
+    {
+        // Freeze time and stop input
+
+        // Show respawn UI
+        if (respawnPanel != null)
+            respawnPanel.SetActive(true);
+
+        // Wait until the player releases any input (holding down buttons won't trigger a restart)
+        while (Input.GetMouseButton(0) || Input.touchCount > 0)
+        {
+            yield return null;
+        }
+
+        // Wait for a fresh tap or click
+        bool tapped = false;
+        while (!tapped)
+        {
+            // Check for new input after releasing previous input
+            if (Input.GetMouseButtonDown(0) || Input.touchCount > 0)
+            {
+                tapped = true;
+            }
+            yield return null;
+        }
+
+        // Unfreeze time
+        Time.timeScale = 1f;
+
+        // Move the player to the last checkpoint
+        transform.position = lastCheckpointPosition;
+
+        // Hide the respawn panel
+        if (respawnPanel != null)
+            respawnPanel.SetActive(false);
+
+        // Reset the level after respawn
+        LevelResetManager.Instance.ResetLevel();
+    }
 }
