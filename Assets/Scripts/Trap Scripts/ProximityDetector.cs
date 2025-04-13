e7b000caa557d5f2693de1a6280cd7de8be063a0
Clyoth <sumiplorem@gmail.com>
2025-04-13 14:23:09 +0800

    Implemented auto-targeting for player in ProximityDetector

--- Assets/Scripts/Trap Scripts/ProximityDetector.cs
+++ Assets/Scripts/Trap Scripts/ProximityDetector.cs
@@ -3,14 +3,29 @@ using UnityEngine.Events;
 
 public class ProximityDetector : MonoBehaviour
 {
-    [SerializeField] private Transform player;
+    private string playerTag = "Player";
     [SerializeField] private UnityEvent onPlayerEnter;
+
+    private Transform player;
+
+    private void Start()
+    {
+        GameObject foundPlayer = GameObject.FindGameObjectWithTag(playerTag);
+        if (foundPlayer != null)
+        {
+            player = foundPlayer.transform;
+        }
+        else
+        {
+            Debug.LogWarning("ProximityDetector: No GameObject with tag '" + playerTag + "' found.");
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.transform == player)
         {
-            onPlayerEnter.Invoke(); 
+            onPlayerEnter.Invoke();
         }
     }
 }
