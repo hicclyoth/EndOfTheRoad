f2394313d6b0b9bed07beef7db896766d0b785d0
Clyoth <sumiplorem@gmail.com>
2025-04-24 13:23:46 +0800

    Fix traps and improve animations
    - Added support for reversed trap animations
    - Ensured all animations reset properly on respawn
    - Fixed a broken spike trap that wasn't triggering correctly

--- Assets/Scripts/Trap Scripts/AnimationTrigger.cs
+++ Assets/Scripts/Trap Scripts/AnimationTrigger.cs
@@ -1,17 +1,31 @@
 using UnityEngine;
 
-public class AnimationTrigger : MonoBehaviour
+public class AnimationTrigger : MonoBehaviour, IResettable
 {
     private Animator animator;
+
+    [SerializeField] private string idleStateName = "Idle";
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
     }
 
+    private void Start()
+    {
+        if (LevelResetManager.Instance != null)
+        {
+            LevelResetManager.Instance.Register(this);
+        }
+    }
 
     public void PlayOnceAnimation()
     {
         animator.SetTrigger("PlayOnce");
+    }
+
+    public void ResetState()
+    {
+        animator.Play(idleStateName, 0, 0f);
     }
 }
